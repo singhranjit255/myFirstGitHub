a17951af66b6843d18425ecb481e5203b8d0525a
singhranjit255 <52627040+singhranjit255@users.noreply.github.com>
2019-07-07 09:14:02 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,12 @@
+public class main
+{
+public static void main(String[]args)
+ {
+ /**************************
+  write a program that adds a to B and store result in C
+  where A = 3, B = 4, C = 5
+  *************************/
+  }// end of of main
+}//end of class
+
+}
